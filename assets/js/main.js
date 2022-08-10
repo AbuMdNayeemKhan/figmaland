a57b8c0b301ec3de54548504d1e10c879225df82
Abu Md Nayeem Khan <abumdnayeemkhan@gmail.com>
2022-08-10 15:46:57 +0600

    some issues solved

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,20 @@
+//==========Script for video section start==========// 
+$(".video")
+  .parent()
+  .click(function () {
+    if ($(this).children(".video").get(0).paused) {
+      $(this).children(".video").get(0).play();
+      $(this).children(".playpause").fadeOut();
+    } else {
+      $(this).children(".video").get(0).pause();
+      $(this).children(".playpause").fadeIn();
+    }
+});
+//==========Script for video section end==========//
+
+
+
+
 //==========Script for hamburger manu start==========//
 let navList = document.querySelector(".nav-section-wrapper-bar");
 let togglerBtn = document.querySelector(".nav-section-wrapper-toggler");
@@ -11,3 +28,4 @@ navList.addEventListener("click", () => {
     navList.classList.remove("active");
 })
 //==========Script for hamburger manu end==========//
+
